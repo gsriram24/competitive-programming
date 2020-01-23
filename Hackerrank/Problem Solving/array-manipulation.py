fd1604a967f7e4653f0233341659081df5fbc340
G Sriram <sgcuber24@gmail.com>
2020-01-23 17:21:55 +0530

    Optimal solution for array manipulation

--- Hackerrank/Problem Solving/array-manipulation.py
+++ Hackerrank/Problem Solving/array-manipulation.py
@@ -1,7 +1,16 @@
 n, m = map(int, input().split())
 arr = [0]*n
+
 for _ in range(m):
     q = list(map(int, input().split()))
-    for i in range(q[0]-1, q[1]):
-        arr[i] += q[2]
-print(max(arr))
+    # for i in range(q[0]-1, q[1]):
+    #     arr[i] += q[2]
+    arr[q[0]-1] += q[2]
+    if q[1] != len(arr):
+        arr[q[1]] -= q[2]
+sum1 = 0
+max1 = 0
+for i in arr:
+    sum1 += i
+    max1 = max(max1, sum1)
+print(max1)
